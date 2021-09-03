f23df7cbe0adfe164ae42498de8d3f119e4c7cae
jordydave <jordydave1@gmail.com>
2021-09-03 13:21:03 +0700

    first commit

---
+++ lib/ui/settings_page.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:dicoding_news_app/provider/preferences_provider.dart';
+import 'package:dicoding_news_app/provider/scheduling_provider.dart';
+import 'package:dicoding_news_app/widgets/custom_dialog.dart';
+import 'package:dicoding_news_app/widgets/platform_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  static const String settingsTitle = 'Settings';
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(settingsTitle),
+      ),
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(settingsTitle),
+      ),
+      child: _buildList(context),
+    );
+  }
+
+  Widget _buildList(BuildContext context) {
+    return Consumer<PreferencesProvider>(
+      builder: (context, provider, child) {
+        return ListView(
+          children: [
+            Material(
+              child: ListTile(
+                title: Text('Dark Theme'),
+                trailing: Switch.adaptive(
+                  value: provider.isDarkTheme,
+                  onChanged: (value) {
+                    provider.enableDarkTheme(value);
+                  },
+                ),
+              ),
+            ),
+            Material(
+              child: ListTile(
+                title: Text('Scheduling News'),
+                trailing: Consumer<SchedulingProvider>(
+                  builder: (context, scheduled, _) {
+                    return Switch.adaptive(
+                      value: provider.isDailyNewsActive,
+                      onChanged: (value) async {
+                        if (Platform.isIOS) {
+                          customDialog(context);
+                        } else {
+                          scheduled.scheduledNews(value);
+                          provider.enableDailyNews(value);
+                        }
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+}
---
+++ lib/widgets/card_article.dart
@@ -0,0 +1,56 @@
+import 'package:dicoding_news_app/common/navigation.dart';
+import 'package:dicoding_news_app/data/model/article.dart';
+import 'package:dicoding_news_app/provider/database_provider.dart';
+import 'package:dicoding_news_app/ui/article_detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CardArticle extends StatelessWidget {
+  final Article article;
+
+  const CardArticle({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        return FutureBuilder<bool>(
+          future: provider.isBookmarked(article.url),
+          builder: (context, snapshot) {
+            var isBookmarked = snapshot.data ?? false;
+            return Material(
+              child: ListTile(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                leading: Hero(
+                  tag: article.urlToImage!,
+                  child: Image.network(
+                    article.urlToImage!,
+                    width: 100,
+                  ),
+                ),
+                title: Text(
+                  article.title,
+                ),
+                subtitle: Text(article.author ?? ""),
+                trailing: isBookmarked
+                    ? IconButton(
+                        icon: Icon(Icons.bookmark),
+                        color: Theme.of(context).accentColor,
+                        onPressed: () => provider.removeBookmark(article.url),
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.bookmark_border),
+                        color: Theme.of(context).accentColor,
+                        onPressed: () => provider.addBookmark(article),
+                      ),
+                onTap: () => Navigation.intentWithData(
+                    ArticleDetailPage.routeName, article),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
